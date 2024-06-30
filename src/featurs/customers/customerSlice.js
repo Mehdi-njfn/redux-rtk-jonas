d60abbbe5d994b04be236631dfb91e25bf7b5113
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-06-30 18:55:49 +0330

    19

---
+++ src/featurs/customers/customerSlice.js
@@ -0,0 +1,35 @@
+
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateCustomer":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+function updateCustomer(fullName) {
+  return { type: "customer/updateCustomer", payload: fullName };
+}
+
+export {createCustomer, updateCustomer}
