144741e9c4452815a7886bc26b072b289d2d813a
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-06-30 18:55:24 +0330

    16

---
+++ src/featurs/accounts/BalanceDisplay.js
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+
+function formatCurrency(value) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+function BalanceDisplay() {
+  const balance = useSelector(s=> s.account.balance)
+
+  return <div className="balance">{formatCurrency(balance || 0)}</div>;
+}
+
+export default BalanceDisplay;
