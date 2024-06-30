2fc4d358e7982342359c82ae7ae642c73b795435
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-06-30 18:54:44 +0330

    11

---
+++ src/store-v1.js
@@ -0,0 +1,90 @@
+import { combineReducers, createStore } from "redux";
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+
+    case "account/withdrow":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loanPurpose: action.payload.purpose,
+        loan: action.payload.amount,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateCustomer":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer
+})
+
+const store = createStore(rootReducer);
+store.dispatch(requestLoan(500, "Buy a car"));
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdrow(amount) {
+  return { type: "account/withdrow", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+function updateCustomer(fullName) {
+  return { type: "customer/updateCustomer", payload: fullName };
+}
+
+store.dispatch(createCustomer('mehdi', '336036'))
+
+console.log(store.getState())
